f01a1adebe6f67896fbdce978e8c0f6590b990f2
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-05-23 18:04:00 +0500

    fix: wip page spacing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import WorkInProgress from '@my/components/WIP'
 
 export default function Home() {
    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between px-5">
          <WorkInProgress />
       </main>
    )
