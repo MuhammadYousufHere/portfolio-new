c07c23a69d1d6f7cdcde149ad6d568d6e06ab3df
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-06-11 06:47:30 +0500

    feat: theme provider comp

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,14 +1,19 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
 import React from 'react';
 
 // const preferDarkSchema =
 //    window.matchMedia &&
 //    window.matchMedia('(prefers-color-scheme: dark)').matches;
 // const defaultTheme = preferDarkSchema ? 'dark' : 'light';
-export function Provider({ children }: { children: React.ReactNode }) {
-   return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+export function Provider({ children, ...props }: ThemeProviderProps) {
+   return (
+      <ThemeProvider attribute="class" {...props}>
+         {children}
+      </ThemeProvider>
+   );
 }
 
 export default Provider;
