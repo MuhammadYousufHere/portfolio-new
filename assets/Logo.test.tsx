812a02e3b67785a962b99deb5d044e2e8d2ebc56
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-06-11 06:17:40 +0500

    app static assets

--- assets/Logo.test.tsx
+++ assets/Logo.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { Logo } from './Logo';
 
-describe('BurgerIcon', () => {
+describe('Logo', () => {
    it('renders correctly', () => {
       const { container } = render(<Logo />);
       expect(container).toMatchSnapshot();
