5b22f7788325b3f07fd6af3a95f2adec4edd40bf
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-06-17 20:24:22 +0500

    feat: render the canvas only hero

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { renderCanvas } from '@my/lib/renderCanvas';
 import { motion } from 'framer-motion';
-import { type ReactNode } from 'react';
+import { useEffect, type ReactNode } from 'react';
 import Header from './Header';
 import SectionContainer from './SectionContainer';
 
@@ -15,29 +16,34 @@ export default function MainLayout({ children }: Props) {
       enter: { opacity: 1, x: 0 },
       exit: { opacity: 0, x: 0 },
    };
+   useEffect(() => {
+      renderCanvas();
+   }, []);
 
    return (
-      <div
-         className="h-screen dark:bg-bg-dark "
+      <SectionContainer
+         isScrollSection
+         className="h-screen dark:bg-bg-dark"
          style={{
             backgroundImage:
                'linear-gradient(38.73deg, rgba(204, 0, 187, 0.25) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.25) 100%)',
          }}
       >
-         <SectionContainer>
-            <Header />
-            <motion.main
-               className="mb-auto bg-transparent"
-               initial="hidden"
-               animate="enter"
-               exit="exit"
-               variants={variants}
-               transition={{ type: 'linear' }}
-            >
-               {children}
-            </motion.main>
-            {/* <Footer /> */}
-         </SectionContainer>
-      </div>
+         <Header />
+         <motion.main
+            className="mb-auto bg-transparent"
+            initial="hidden"
+            animate="enter"
+            exit="exit"
+            variants={variants}
+            transition={{ type: 'linear' }}
+         >
+            <canvas
+               className="bg-skin-base pointer-events-none absolute inset-0"
+               id="canvas"
+            />
+            {children}
+         </motion.main>
+      </SectionContainer>
    );
 }
