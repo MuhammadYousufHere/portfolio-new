bacbd7426ab0e43639655513bfc2c8b5ad52bb09
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-06-17 22:23:30 +0500

    wip: blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import WIP from '@my/components/WIP';
+import React from 'react';
+
+export default function Blogs() {
+   return (
+      <div className="h-screen flex items-center justify-center">
+         <WIP withDesc={false} />;
+      </div>
+   );
+}
