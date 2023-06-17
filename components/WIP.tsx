d5f1e78e3e6741444a69b2e095d84853a1bc7914
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-06-17 22:29:38 +0500

    enhance wip component with prop

--- components/WIP.tsx
+++ components/WIP.tsx
@@ -1,14 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 
-'use client';
-
-// import { renderCanvas } from '@my/lib/renderCanvas';
-// import React, { useEffect } from 'react';
-
-export default function WIP() {
-   // useEffect(() => {
-   //    renderCanvas();
-   // }, []);
+export default function WIP({ withDesc }: { withDesc: boolean }) {
    return (
       <div className="flex items-center justify-center bg-transparent mt-10">
          <div className="flex flex-col items-center justify-center max-w-2xl">
@@ -20,15 +12,13 @@ export default function WIP() {
             <h1 className="my-3 text-3xl font-bold text-center dark:text-yellow-300 text-gray-900">
                Under Construction
             </h1>
-            <p className="text-center text-gray-700 text-md dark:text-gray-200">
-               The website is currently under construction and will be live
-               soon.
-            </p>
+            {withDesc && (
+               <p className="text-center text-gray-700 text-md dark:text-gray-200">
+                  The website is currently under construction and will be live
+                  soon.
+               </p>
+            )}
          </div>
-         {/* <canvas
-            className="bg-skin-base pointer-events-none absolute inset-0"
-            id="canvas"
-         /> */}
       </div>
    );
 }
