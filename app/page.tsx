2b0a5b43bd28ac75681e1d55b430f0877c5ed0df
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-06-18 20:13:42 +0500

    wip: app build

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import MainHeading from '@my/common/MainHeading';
 import Advert from '@my/components/Advert';
-import { Footer } from '@my/components/Footer';
 import MainLayout from '@my/components/MainContainer';
 import NavbarContainer from '@my/components/NavbarContainer';
 // import TestGSAP from '@my/components/Projects/Test';
@@ -22,8 +20,6 @@ export default function Home() {
       <ScrollProvider>
          <MainLayout>
             <NavbarContainer />
-
-            <MainHeading />
          </MainLayout>
          <SectionContainer isScrollSection>
             <Advert />
@@ -33,12 +29,6 @@ export default function Home() {
          </SectionContainer>
          <SectionContainer isScrollSection>
             <Skills />
-         </SectionContainer>
-         <SectionContainer
-            isScrollSection
-            className="sm:px-0 md:px-0 sm:mx-0 lg:mx-0 lg:max-w-full sm:max-w-full md:max-w-full lg:px-0"
-         >
-            <Footer />
          </SectionContainer>
       </ScrollProvider>
    );
