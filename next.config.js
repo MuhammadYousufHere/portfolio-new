8bcabc1a617c74769b66da46bc92598b3c30e650
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-06-24 09:35:10 +0500

    chores: update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    output: 'export',
-   images: { unoptimized: true },
+   images: {
+      unoptimized: true,
+   },
 };
 
 module.exports = nextConfig;
