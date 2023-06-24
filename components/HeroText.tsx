b6c64a46448f934b0983c6269ea175ee09134441
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-06-24 10:06:14 +0500

    wip: new hero text compnent

---
+++ components/HeroText.tsx
@@ -0,0 +1,173 @@
+import React from 'react';
+
+export default function HeroText() {
+   return (
+      <div className="-translate-y-2/4 absolute w-full h-fit z-[9991] left-0 top-2/4 font-syne ">
+         <div className="flex justify-center relative">
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[10vw] text-[#DAD9D5] font-black overflow-hidden h">
+                  H
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[10vw] text-[#DAD9D5] font-black overflow-hidden e">
+                  E
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[10vw] text-[#DAD9D5] font-black overflow-hidden">
+                  L
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[10vw] text-[#DAD9D5] font-black overflow-hidden">
+                  L
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[10vw] text-[#DAD9D5] font-black overflow-hidden">
+                  O
+               </div>
+            </div>
+         </div>
+         <div className="flex justify-center relative  mt-[-3%]">
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5]  overflow-hidden">
+                  H
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5]  overflow-hidden">
+                  O
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5]  overflow-hidden">
+                  W
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div
+                  className="text-[5vw] text-[#DAD9D5]  overflow-hidden"
+                  style={{ opacity: 0 }}
+               >
+                  -
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  A
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  R
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  E
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div
+                  className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden"
+                  style={{ opacity: 0 }}
+               >
+                  -
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  Y
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  O
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  U
+               </div>
+            </div>
+         </div>
+         <div className="flex justify-center relative mt-[-2%]">
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  F
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  E
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  E
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  L
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  I
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  N
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  G
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div
+                  className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden"
+                  style={{ opacity: 0 }}
+               >
+                  -
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  T
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  O
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  D
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  A
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  Y
+               </div>
+            </div>
+            <div className="block text-start relative overflow-hidden">
+               <div className="text-[5vw] text-[#DAD9D5] font-syne font-medium overflow-hidden">
+                  ?
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+}
