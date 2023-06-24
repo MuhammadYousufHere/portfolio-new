77302b94fcfd1730d7f223f17bc5b8360f0b96e9
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-06-24 09:54:10 +0500

    chores: export files

---
+++ lib/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
+
+// const preferDarkSchema =
+//    window.matchMedia &&
+//    window.matchMedia('(prefers-color-scheme: dark)').matches;
+// const defaultTheme = preferDarkSchema ? 'dark' : 'light';
+export function Provider({ children, ...props }: ThemeProviderProps) {
+   return (
+      <ThemeProvider attribute="class" {...props}>
+         {children}
+      </ThemeProvider>
+   );
+}
+
+export default Provider;
---
+++ lib/index.ts
@@ -0,0 +1,1 @@
+export { titleAnimation } from './titleAnimation';
