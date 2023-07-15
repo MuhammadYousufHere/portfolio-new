82e2774dd6e2f6f89f332acd3e7269b924c59cff
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-07-15 20:04:19 +0500

    update: header ui changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
-import { Logo } from '@my/assets';
+import { LogoFill } from '@my/assets/LogoFill';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { useLocomotiveScroll } from 'react-locomotive-scroll';
+// import { useEffect, useState } from 'react';
+// import { useLocomotiveScroll } from 'react-locomotive-scroll';
 import NavbarContainer from './NavbarContainer';
 import { ThemeSwitch } from './ThemeSwitch';
 
@@ -12,26 +12,26 @@ export const headerNavLinks = [
    { href: '/skills', title: 'Skills' },
    // { href: '/blog', title: 'Blog' },
 ];
-type State = {
-   about?: Element;
-   skills?: Element;
-};
+// type State = {
+//    about?: Element;
+//    skills?: Element;
+// };
 export default function Header() {
-   const { scroll } = useLocomotiveScroll();
-   const [sections, setSections] = useState<State>({});
-   useEffect(() => {
-      if (!scroll) return;
+   // const { scroll } = useLocomotiveScroll();
+   // const [sections, setSections] = useState<State>({});
+   // useEffect(() => {
+   //    if (!scroll) return;
 
-      const about = document.querySelector('[data-scroll-id="about"]')!;
-      const skills = document.querySelector('[data-scroll-id="skills"]')!;
-      setSections({ about, skills });
-   }, [scroll]);
-   const scrollTo = (sectionName: string) => {
-      if (!sectionName) return;
-      scroll.scrollTo(sections[sectionName as keyof State], {
-         duration: 500,
-      });
-   };
+   //    const about = document.querySelector('[data-scroll-id="about"]')!;
+   //    const skills = document.querySelector('[data-scroll-id="skills"]')!;
+   //    setSections({ about, skills });
+   // }, [scroll]);
+   // const scrollTo = (sectionName: string) => {
+   //    if (!sectionName) return;
+   //    scroll.scrollTo(sections[sectionName as keyof State], {
+   //       duration: 500,
+   //    });
+   // };
 
    return (
       <header className="z-40 bg-transparent pt-2 md:py-10 md:pb-20">
@@ -43,7 +43,7 @@ export default function Header() {
                   aria-label="Home"
                >
                   <div className="w-10 h-10">
-                     <Logo />
+                     <LogoFill />
                   </div>
                </Link>
                <div className="flex items-center space-x-3 text-base leading-5 z-50">
@@ -51,9 +51,11 @@ export default function Header() {
                      {headerNavLinks.map(({ title }) => (
                         <button
                            type="button"
-                           onClick={() => scrollTo(title.toLocaleLowerCase())}
+                           // onClick={() => scrollTo(title.toLocaleLowerCase())}
+                           // eslint-disable-next-line @typescript-eslint/no-empty-function
+                           onClick={() => {}}
                            key={title}
-                           className="basic-text py-1 after:bg-black dark:after:bg-white underline-animation text-md font-semibold dark:text-white"
+                           className="basic-text py-1 after:bg-black dark:after:bg-white underline-animation text-md font-semibold dark:text-white text-slate-900"
                            aria-label={title}
                         >
                            <span className="font-medium tracking-wide text-gray-00 dark:text-gray-100">
