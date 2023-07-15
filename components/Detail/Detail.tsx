d802684e456545ff86ccc84e70fcd3f2276c437a
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-07-15 20:23:04 +0500

    wip: more details page section

---
+++ components/Detail/Detail.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Detail() {
+   return <div className="h-screen dark:bg-white text-dark sticky">Detail</div>;
+}
---
+++ components/Detail/index.ts
@@ -0,0 +1,1 @@
+export { default as MyDetails } from './Detail';
