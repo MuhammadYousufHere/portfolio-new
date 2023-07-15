9cba253b02b47a7761579c81f2dccd881cae9981
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-07-15 20:02:11 +0500

    wip: app cookie banner

---
+++ components/Cookies.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Cookies() {
+   return (
+      <div>
+         We use cookies to provide website functionality, to analyze traffic on
+         our website and to enable social media functionality. Our Privacy
+         Policy provides more information.
+         <span>Accept all</span>
+         <span>Reject all</span>
+      </div>
+   );
+}
